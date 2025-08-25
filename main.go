410fd804ed80217cae328b938bd65add5244e049
Rafael De Moraes Lima <rafaelmrslima@gmail.com>
2025-08-25 23:32:12 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+)
+
+type Coin struct {
+	Coin string
+	Usd  float64
+}
+
+const URL string = "https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd"
+
+var allCoins = []string{"bitcoin", "ethereum", "cardano", "chainlink", "solana", "polkadot"}
+
+func main() {
+
+	fmt.Println("Available coins:")
+	for _, coin := range allCoins {
+		fmt.Println("-", coin)
+	}
+	fmt.Print("Enter coins to fetch (comma-separated, e.g., bitcoin,ethereum): ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	input := strings.TrimSpace(scanner.Text())
+	coins := strings.Split(input, ",")
+	validCoins := make([]string, len(coins))
+
+	for _, coin := range coins {
+		coinIsAvailable := false
+		for _, availableCoin := range allCoins {
+			if strings.ToLower(coin) == availableCoin {
+				coinIsAvailable = true
+				validCoins = append(validCoins, coin)
+				break
+			}
+		}
+
+		if !coinIsAvailable {
+			fmt.Println("coin not found, being ignored:", coin)
+		}
+	}
+
+	data, err := getPrices(validCoins)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println(data)
+}
+
+func getPrices(coinsToFetch []string) ([]Coin, error) {
+	var wg sync.WaitGroup
+	coinsPrices := make([]Coin, len(coinsToFetch))
+	wg.Add(len(coinsToFetch))
+
+	for i, coin := range coinsToFetch {
+		go func() {
+			price, err := fetchPrice(coin)
+			if err != nil {
+				fmt.Printf("Error fetching %s: %v\n", coin, err)
+				return
+			}
+			coinsPrices[i] = Coin{Coin: coin, Usd: price}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	return coinsPrices, nil
+}
+
+func fetchPrice(coin string) (float64, error) {
+	resp, err := http.Get(URL + "&ids=" + coin)
+
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	var result map[string]map[string]float64
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	return result[coin]["usd"], nil
+}
