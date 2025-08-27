0e2937d3bce581faf1ea42707c3496e755841cd1
Rafael De Moraes Lima <rafaelmrslima@gmail.com>
2025-08-27 18:56:16 +0200

    adding saveToFile - csv and json

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"os"
 	"strings"
 	"sync"
+
+	"github.com/gocarina/gocsv"
 )
 
 type Coin struct {
@@ -22,6 +24,21 @@ var allCoins = []string{"bitcoin", "ethereum", "cardano", "chainlink", "solana",
 
 func main() {
 
+	coins := getCoinsUserInput()
+	validCoins := validateCoinsInput(coins)
+	data, err := getPrices(validCoins)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	printPrices(data)
+	saveToFile(data)
+	fmt.Println("Exiting the program.")
+}
+
+func getCoinsUserInput() []string {
 	fmt.Println("Available coins:")
 	for _, coin := range allCoins {
 		fmt.Println("-", coin)
@@ -31,6 +48,10 @@ func main() {
 	scanner.Scan()
 
 	coins := strings.Split(scanner.Text(), ",")
+	return coins
+}
+
+func validateCoinsInput(coins []string) []string {
 	validCoins := []string{}
 
 	for _, coin := range coins {
@@ -43,20 +64,12 @@ func main() {
 				break
 			}
 		}
-
 		if !coinIsAvailable {
 			fmt.Println("coin not found, being ignored:", coin)
 		}
 	}
 
-	data, err := getPrices(validCoins)
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	printPrices(data)
+	return validCoins
 }
 
 func getPrices(coinsToFetch []string) ([]Coin, error) {
@@ -85,6 +98,7 @@ func fetchPrice(coin string) (float64, error) {
 
 	if err != nil {
 		fmt.Println("error: ", err)
+		return 0, err
 	}
 
 	defer resp.Body.Close()
@@ -92,11 +106,13 @@ func fetchPrice(coin string) (float64, error) {
 
 	if err != nil {
 		fmt.Println("error: ", err)
+		return 0, err
 	}
 
 	var result map[string]map[string]float64
 	if err := json.Unmarshal(body, &result); err != nil {
 		fmt.Println("error: ", err)
+		return 0, err
 	}
 
 	return result[coin]["usd"], nil
@@ -110,4 +126,56 @@ func printPrices(prices []Coin) {
 		fmt.Printf("| %-11s | %.2f |\n", p.Coin, p.Usd)
 	}
 	fmt.Println("+-------------+-----------+")
+}
+
+func saveToFile(data []Coin) {
+	fmt.Print("If you want to save the prices, enter 'csv' or 'json'. otherwise write 'exit' to finish the program. ")
+	scannerExport := bufio.NewScanner(os.Stdin)
+	scannerExport.Scan()
+	input := scannerExport.Text()
+
+	if strings.ToLower(input) == "csv" {
+		generateCSV(data)
+	} else if strings.ToLower(input) == "json" {
+		generateJson(data)
+	}
+}
+
+func generateCSV(data []Coin) {
+	file, err := os.Create("data.csv")
+	if err != nil {
+		fmt.Println("Error to generate csv file.", err)
+		return
+	}
+	defer file.Close()
+
+	if err := gocsv.MarshalFile(&data, file); err != nil {
+		fmt.Println("Error to generate csv file.", err)
+		return
+	}
+
+	fmt.Println("CSV file saved successfully!")
+}
+
+func generateJson(data []Coin) {
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error to convert data to json.", err)
+		return
+	}
+
+	file, err := os.Create("data.json")
+	if err != nil {
+		fmt.Println("Error creating json file.", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.Write(jsonBytes)
+	if err != nil {
+		fmt.Println("Error writing to json file.", err)
+		return
+	}
+
+	fmt.Println("JSON file saved successfully!")
 }
